4ca0229e3daf09ecba25d7fb5cf1189fee3c8749
hissoune <khalidhissoune962@gmail.com>
2025-02-10 15:49:50 +0100

    [Redux] Added AppWrapper for centralized state management

---
+++ app/(redux)/AppWrapper.tsx
@@ -0,0 +1,27 @@
+import  { useEffect } from "react";
+import { Stack } from "expo-router";
+import { useAppDispatch } from '@/hooks/useAppDispatch';
+import { loadUser } from "./authSlice";
+
+
+
+const AppWrapper = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+
+    dispatch(loadUser())
+    
+
+  }, [dispatch]);
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
+};
+
+export default AppWrapper;
