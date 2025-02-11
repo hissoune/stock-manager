fa04b9adf2d6d2d89252165667ea7c838907e046
hissoune <khalidhissoune962@gmail.com>
2025-02-11 20:47:19 +0100

    [Redux] Updated productsSlice for stock updates

--- app/(redux)/productsSlice.ts
+++ app/(redux)/productsSlice.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { getProducts } from "../(services)/api/productsApi";
+import { getProducts, UpdateQuantity } from "../(services)/api/productsApi";
 import { Product } from "@/constants/types";
 
 
@@ -8,6 +8,16 @@ export const loadProducts = createAsyncThunk(
     async ()=>{
         const response = await getProducts();
         return response;
+    }
+);
+
+export const updateQuantity =createAsyncThunk(
+    "products/stoks/duantity",
+    async ({ type, productId, stokId }: { type: string; productId: string|undefined; stokId: number }) => {
+        const updatedProduct = await UpdateQuantity(type, productId, stokId);
+        return updatedProduct;
+      
+        
     }
 );
 
@@ -43,6 +53,16 @@ const productSlice = createSlice({
         .addCase(loadProducts.rejected, (state)=>{
             state.isLoadind=false;
             state.error="Error";
+        })
+        .addCase(updateQuantity.pending, (state)=>{
+            state.isLoadind= false
+        })
+        .addCase(updateQuantity.fulfilled, (state,action)=>{
+            state.isLoadind =false;
+            state.product = action.payload;
+        })
+        .addCase(updateQuantity.rejected, (state)=>{
+            state.error = "error while updating the stoke "
         })
     },
 })
