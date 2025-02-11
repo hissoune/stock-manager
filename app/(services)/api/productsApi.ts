30c2398f3eaaca8c7522f0eb31b2089d0d15b5ce
hissoune <khalidhissoune962@gmail.com>
2025-02-11 20:47:20 +0100

    [API] Updated productsApi for stock management

--- app/(services)/api/productsApi.ts
+++ app/(services)/api/productsApi.ts
@@ -1,3 +1,4 @@
+import { stok } from '../../../constants/types';
 
 
 export  const getProducts = async () => {
@@ -12,3 +13,33 @@ export  const getProducts = async () => {
     const data = await response.json();
     return data;
 }
+   export const UpdateQuantity = async (type:string,productId:string|undefined,stokId:number)=>{
+
+            const response = await fetch(`${process.env.EXPO_PUBLIC_URL}/products/${productId}`);
+
+            const product =await response.json();
+
+            let updatedStocks=[]
+             if (product) {
+                switch (type){
+              
+                    case 'add':
+                        updatedStocks = product.stocks.map((stock:stok) => 
+                            stock.id === stokId ? { ...stock, quantity: stock.quantity + 1 } : stock
+                          );
+                     break;
+                     case 'remove':
+                        updatedStocks = product.stocks.map((stock:stok) => 
+                            stock.id === stokId ? { ...stock, quantity:stock.quantity -1 } : stock
+                          );
+                          break;
+                  }
+             }
+             
+             const updatedProduct = await fetch(`${process.env.EXPO_PUBLIC_URL}/products/${productId}`,{
+                method: "PATCH",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ stocks: updatedStocks })
+             });
+                return updatedProduct.json();
+   }
